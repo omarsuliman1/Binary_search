dd4c2f2016fdd4ce8ffe2838434ac0d41d1b0029
Omar Suliman <omarsuliman294@gmail.com>
2026-01-21 21:59:03 +0200

    Refactor E_E.cpp to simplify main function logic

--- E_E.cpp
+++ E_E.cpp
@@ -1,47 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define Sonic ios::sync_with_stdio(false); cin.tie(nullptr);
-#define int long long
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-void fun() {
-    int n, x;
-    cin >> n >> x;
+    int n, m;
+    cin >> n >> m;
 
-    vector<pair<int,int>> a(n);
-    for(int i = 0; i < n; i++){
-        cin >> a[i].first;
-        a[i].second = i + 1; 
+    vector<long long> a(n), b(m);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < m; i++) cin >> b[i];
+
+    long long r = 0;
+    int j = 0;
+
+    for (int i = 0; i < n; i++) {
+        while (j < m-1 && abs(b[j+1] - a[i]) <= abs(b[j] - a[i]))
+            j++;
+        r = max(r, abs(b[j] - a[i]));
     }
 
-    sort(a.begin(), a.end());
-
-    int l = 0, r = n - 1;
-    while(l < r){
-        long long s = a[l].first + a[r].first;
-
-        if(s == x){
-            int g = a[l].second;
-            int v = a[r].second;
-           
-            if(g < v){
-
-             cout << g << " " << v << "\n";
-            }
-            else 
-            cout << v << " " << g << "\n";
-
-            return;
-        }
-
-        if(s < x) l++;
-        else r--;
-    }
-
-    cout << "IMPOSSIBLE\n";
+    cout << r << "\n";
 }
 
-signed main(){
-    Sonic
-    fun();
-}

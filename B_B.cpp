d820e0f8c6d82aa38f007c11dcd1c909524c9250
Omar Suliman <omarsuliman294@gmail.com>
2026-01-21 21:57:48 +0200

    Update B_B.cpp

--- B_B.cpp
+++ B_B.cpp
@@ -1,24 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define Sonic ios::sync_with_stdio(false); cin.tie(nullptr);
-#define int long long
-#define all(v) (v).begin(), (v).end()
-void fun(){
-    string s;
-    cin >> s;
- int mx = 1, cur = 1;
- for(int i = 1; i < s.size(); i++){
-        if(s[i] == s[i-1]) cur++;
-        else cur = 1;
-        mx = max(mx, cur);
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<int> x(n);
+    for (int i = 0; i < n; i++)
+        cin >> x[i];
+
+    sort(x.begin(), x.end());
+
+    int q;
+    cin >> q;
+
+    while (q--) {
+        long long m;
+        cin >> m;
+ 
+        int cnt = upper_bound(x.begin(), x.end(), m) - x.begin();
+        cout << cnt << "\n";
     }
-
-    cout << mx << "\n";
-}
-
-signed main(){
-    Sonic
-    int t = 1;
-    // cin >> t;
-    while(t--) fun();
 }
